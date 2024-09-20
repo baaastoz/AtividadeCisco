72e9dd4e82daaf1787643c215f0de6256248e9cd
Vinicius <128731182+baaastoz@users.noreply.github.com>
2024-09-20 09:53:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function calcular(){
-    var val1 = parseFloat(document.getElementById('val1').value);
-    var val2 = parseFloat(document.getElementById('val2').value);
-    var val2 = parseFloat(document.getElementById('val3').value);
-
     /* parseFloat: converte a string recebida como argumento e a retorna como um número */
-    var media = (val1 + val2 + val3) / 3;
-    document.getElementById('resultado').innerText = media;
+    const k = 8.9875 * Math.pow(10, 9);
+    const q1 = parseFloat(document.getElementById('q1').value);
+    const q2 = parseFloat(document.getElementById('q2').value);
+    const D = parseFloat(document.getElementById('D').value);
 
+
+    const result = k * Math.abs(q1 * q2) / Math.pow(D, 2);
+
+    document.getElementById('result').innerText = result.toExponential(2);
 }
+
